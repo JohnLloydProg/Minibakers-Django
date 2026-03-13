5a4bfae146df24e66a1539b27a13a3502a6a6e5d
jajames23 <jamiedelrosario321@gmail.com>
2026-03-13 18:18:03 +0800

    red

--- minibakers/settings.py
+++ minibakers/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+
+
 SECRET_KEY = 'django-insecure-hb$u^*6hiy7*sf*qk=2&p=ls#3^t$t*in^xzqc&@b1hkf@(69b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
